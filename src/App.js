3278a6b1996283a5cecac7ecd6370f96c588de73
jose asan <joseasan09@gmail.com>
2023-03-08 01:09:26 -0300

    ItemListConteiner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import ItemListContainer from './componets/ItemListContainer/ItemListVontainer';
 import NavBar from './componets/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+
+      <ItemListContainer greeting={"Bienvenidos a BlueMain Online"}/>
     </div>
   );
 }
---
+++ src/componets/ItemListContainer/ItemListVontainer.js
@@ -0,0 +1,8 @@
+const ItemListContainer = ({ greeting }) => {
+    
+    return (
+        <div class="p-3 mb-2 bg-primary text-white border border-5 rounded-pill">{greeting}</div>
+    )
+}
+
+export default ItemListContainer 
