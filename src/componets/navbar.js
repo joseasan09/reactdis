902e4b2e1739a6b136967638584833b8d30d0527
jose asan <joseasan09@gmail.com>
2023-03-07 23:34:34 -0300

    navbar

---
+++ src/componets/navbar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <div class="d-grid gap-2">
+                <button class="btn btn-outline-success" type="button">Dispenser</button>
+                <button class="btn btn-outline-success" type="button">Filtros</button>
+                <button class="btn btn-outline-success" type="button">Mangueras</button>
+            </div>
+      </nav>
+    )
+
+}
+export default NavBar
